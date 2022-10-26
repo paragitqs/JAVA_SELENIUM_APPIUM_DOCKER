bfddca3c02003efd95979a89959ae069cf40f314
paragitqs <parag.ghoshal@gmail.com>
2022-10-26 03:24:02 +0000

    initial java

---
+++ Test_All.java
@@ -0,0 +1,18 @@
+public class Test_All{
+     int ad =5;
+     public int add(){
+
+          return ad+10;
+     }
+
+     public static void main(String[] args){
+
+          Test_All abc = new Test_All();
+          System.out.println("#######" + abc.add());
+          
+           
+
+
+     }
+
+}
